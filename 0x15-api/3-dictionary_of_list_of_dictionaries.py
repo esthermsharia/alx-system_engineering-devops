c96bb9e9869fdd83df4dd04924d479c7bd9c9dd1
kimengu-david <davidmutune200@gmail.com>
2021-05-03 17:14:55 +0000

    Export all employee data to JSON

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,12 +10,11 @@ if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     users = requests.get(url + "users").json()
     with open("todo_all_employees.json", "w") as json_file:
-        for user in users:
-            json.dump({user.get("id"): [{
-                "task": todo.get("title"),
-                "completed": todo.get("completed"),
-                "username": user.get("username")
-            }for todo in requests.get(url + "todos",
-                                      params={"userId": user.get("id")}
-                                      ).json()]},
-                      json_file)
+        json.dump({user.get("id"): [{
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": user.get("username")
+        }for todo in requests.get(url + "todos",
+                                  params={"userId": user.get("id")}
+                                  ).json()]for user in users},json_file)
+        
