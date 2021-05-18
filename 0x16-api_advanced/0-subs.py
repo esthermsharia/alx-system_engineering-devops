467087232487ba5c5d266b83c376e6c7662b7459
kimengu-david <davidmutune200@gmail.com>
2021-05-18 19:12:35 +0000

    Add custom headers to request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns total subscribers  for a specified subreddit"""
+    headers = {
+        "User-Agent": "linux:alx-holberton.advanced_api"
+        }
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    resp = requests.get(url, allow_redirects=False)
+    resp = requests.get(url, headers=headers, allow_redirects=False)
     if resp.status_code == 404:
         return 0
     subscribers = (resp.json().get("data")).get("subscribers")
